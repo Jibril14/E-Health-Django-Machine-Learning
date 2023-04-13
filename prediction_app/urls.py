631aff50ef57fb15006199398a374c8ae63f8ff1
jibril14 <abdullahiabdulwasiu1@gmail.com>
2023-04-13 10:37:55 +0100

    Add Diabetes Prediction Functionality

--- prediction_app/urls.py
+++ prediction_app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from prediction_app import views
 
-urlpatterns = [ 
-     path('', views.index, name="home"),
-    
+urlpatterns = [
+    path(
+        '',
+        views.index,
+        name="home"),
+    path(
+        'check/diabetes/',
+        views.diabetes_prediction,
+        name="diabetes_prediction"),
 ]
--- prediction_app/views.py
+++ prediction_app/views.py
@@ -1,4 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.contrib.auth.decorators import login_required
+from .models import DiabetesPrediction
+import pandas as pd
+from data_model.data_model import make_prediction
+
+
 
 def index(request):
     return render(request, "index.html")
+
+
+@login_required
+def diabetes_prediction(request):
+    make_prediction()
+    if request.method == "POST":
+
+        age = float(request.POST.get("age"))
+        sex = float(request.POST.get("sex"))
+        bmi = float(request.POST.get("bmi"))
+        bp = float(request.POST.get("bp"))
+        tc = float(request.POST.get("tc"))
+        ldl = float(request.POST.get("ldl"))
+        hdl = float(request.POST.get("hdl"))
+        tch = float(request.POST.get("tch"))
+        ltg = float(request.POST.get("ltg"))
+        glucose = float(request.POST.get("glucose"))
+        user = request.user
+
+        diabetes_model = pd.read_pickle("data_model/model.pickle")
+        result = diabetes_model.predict(
+            [[age, sex, bmi, bp, tc, ldl, hdl, tch, ltg, glucose]])
+        if result >= 0.75:
+            result = "Diabetic"
+        else:
+            result = "No Diabetes"
+
+        prediction = DiabetesPrediction(
+            age=age,
+            sex=sex,
+            bmi=bmi,
+            bp=bp,
+            tc=tc,
+            ldl=ldl,
+            hdl=hdl,
+            tch=tch,
+            ltg=ltg,
+            glucose=glucose,
+            result=result,
+            user=user)
+        prediction.save()
+
+        #return HttpResponseRedirect(reverse("diabetes_result"))
+
+    return render(request, "prediction/diabetes.html")
+
